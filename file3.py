41cd01fdf0a1114841e9cb92555ac1c4c84cd55f
apoor <apoorvsri1909gmail.com>
2021-03-23 07:52:51 +0530

    commit done

--- file3.py
+++ file3.py
@@ -1,2 +1,17 @@
-print("Fluxgate Gradiometer")
-print("Electric propulsion")
+class Fan:
+    rpm=100
+    def __init__(self,a):
+        self.dial=a
+    def up(self):
+        self.dial+=1
+        print("Fan speed is:", (self.rpm) * (self.dial))
+    def down(self):
+        self.dial-=1
+        print("Fan speed is:", (self.rpm) * (self.dial))
+
+
+
+
+fan1=Fan(1)
+fan1.up()
+fan1.down()
